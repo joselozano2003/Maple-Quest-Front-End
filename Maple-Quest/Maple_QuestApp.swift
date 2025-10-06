39bea1a010888ed98c695a59d0240979364b8647
Jose Lozano <josecamilo.lozanocet@ucalgary.ca>
2025-10-06 13:01:39 -0600

    Initial Commit

---
+++ Maple-Quest/Maple_QuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maple_QuestApp.swift
+//  Maple-Quest
+//
+//  Created by Jose Lozano on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Maple_QuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
