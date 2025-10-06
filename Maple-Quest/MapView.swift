46ef5e57ad0181c44126d5c86851917273f4ffd4
Camila Hernandez <camila.hernandez@ucalgary.ca>
2025-10-06 16:40:58 -0600

    Created MapView.swift file and added a standard map

---
+++ Maple-Quest/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  Maple-Quest
+//
+//  Created by Camila Hernandez on 2025-10-06.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
