69bb822320bfaea7f337b337879b486d4087318c
Camila Hernandez <camila.hernandez@ucalgary.ca>
2025-10-06 21:32:46 -0600

    Added some padding to HomeView

--- Maple-Quest/HomeView.swift
+++ Maple-Quest/HomeView.swift
@@ -10,27 +10,38 @@ import SwiftUI
 struct HomeView: View {
     
     var body: some View {
-        // Home view title
         VStack(alignment: .leading) {
-            Text("Explore")
-            HStack {
-                Text("Beautiful").bold()
-                Text("Canada!").bold().foregroundColor(.red)
+            
+            // Title Section
+            VStack(alignment: .leading) {
+                Text("Explore")
+                HStack {
+                    Text("Beautiful").bold()
+                    Text("Canada!").bold().foregroundColor(.red)
+                }
+            }
+            .font(.largeTitle)
+            .padding(.top, 50)
+            
+            VStack(alignment: .leading) {
+                // Mini summary section
+                Text("Welcome, ...!")
+                    .font(.system(size: 22)).bold()
+                    .padding(.top)
+                Text("You have visited ... Canadian landmarks!")
+                    .font(.subheadline)
+                // Mini map view of landmarks nearby
+                Text("Explore Landmarks Near You")
+                    .font(.system(size: 22)).bold()
+                    .padding(.top)
+                // Top landmarks in Canada
+                Text("Highlights")
+                    .font(.system(size: 22)).bold()
+                    .padding(.top)
             }
         }
-        .font(.largeTitle)
         .padding()
-        
-        VStack(alignment: .leading) {
-            Text("Welcome, ...!")
-                .font(.system(size: 22)).bold()
-            Text("You have visited ... Canadian landmarks!")
-                .font(.subheadline)
-            Text("Explore Landmarks Near You")
-                .font(.system(size: 22)).bold()
-            Text("Highlights")
-                .font(.system(size: 22)).bold()
-        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
