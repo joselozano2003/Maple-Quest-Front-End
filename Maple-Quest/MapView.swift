9fe169a08ad44e8f0c4892d82d40b9ea569576cf
Camila Hernandez <camila.hernandez@ucalgary.ca>
2025-10-06 18:57:18 -0600

    Added 3 landmarks across Canada

--- Maple-Quest/MapView.swift
+++ Maple-Quest/MapView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    
+    let niagraFalls = CLLocationCoordinate2D(
+        latitude: 43.09468530054129,
+        longitude: -79.03996936268442
+    )
+    
+    let banff = CLLocationCoordinate2D(
+        latitude: 51.497407682404955,
+        longitude: -115.9261679966291
+    )
+    
+    let hopewellRocks = CLLocationCoordinate2D(
+        latitude: 45.817654979524015,
+        longitude: -64.57845776218284
+    )
+    
+    
     var body: some View {
-        Map()
+        Map() {
+            Marker("Niagara Falls", coordinate: niagraFalls)
+            Marker("Banff National Park", coordinate: banff)
+            Marker("HopeWell Rocks Provincial Park", coordinate: hopewellRocks)
+        }
          
     }
 }
