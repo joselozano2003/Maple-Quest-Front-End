adeb66f9fa382cb9260f078ec49c0501675f2d0d
Camila Hernandez <camila.hernandez@ucalgary.ca>
2025-10-10 10:59:11 -0600

    Implemented PhotoGallery.swift to create a grid view for how the photos will be displayed

--- Maple-Quest/Map/PhotoGallery.swift
+++ Maple-Quest/Map/PhotoGallery.swift
@@ -5,3 +5,25 @@
 //  Created by Camila Hernandez on 2025-10-10.
 //
 
+import SwiftUI
+
+struct PhotoGallery: View {
+    let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    @Binding var images: [UIImage]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach(images, id: \.self) { image in
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(10)
+                        .clipped()
+                }
+            }
+            .padding()
+        }
+    }
+}
