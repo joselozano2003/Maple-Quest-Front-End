9a9f9d73fe5e9943d2f5862a0d7d35e38f92fc50
Camila Hernandez <camila.hernandez@ucalgary.ca>
2025-10-13 15:46:36 -0600

    Defaulted to Toronto and have the camera position change depending on user location

--- Maple-Quest/Map/MapPreview.swift
+++ Maple-Quest/Map/MapPreview.swift
@@ -7,21 +7,31 @@
 
 import SwiftUI
 import MapKit
+import Combine
 
 struct MapPreview: View {
     @StateObject private var locationManager = LocationManager()
     @State private var cameraPosition = MapCameraPosition.region(
             MKCoordinateRegion(
-                center: CLLocationCoordinate2D(latitude: 51.0447, longitude: -114.0611), // Default Calgary
+                center: CLLocationCoordinate2D(latitude: 43.6532, longitude: -79.3832),
                 span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
             )
         )
     
     var body: some View {
-        Map(position: $cameraPosition) {}
-        .frame(height: 200)
-        .cornerRadius(15)
-        .shadow(radius: 5)
+        Map(position: $cameraPosition)
+            .frame(height: 200)
+            .cornerRadius(15)
+            .shadow(radius: 5)
+            .onReceive(locationManager.$userLocation.compactMap { $0 }) { loc in
+                // Update camera to user location
+                cameraPosition = MapCameraPosition.region(
+                    MKCoordinateRegion(
+                        center: loc,
+                        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+                    )
+                )
+        }
     }
 }
 
