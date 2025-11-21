7a67d0fd165db1d2201aef8af25095460caffde6
Camila Hernandez <camila.hernandez@ucalgary.ca>
2025-11-21 16:28:42 -0700

    Added comments to LocationDetailView.swift

--- Maple-Quest/Map/LocationDetailView.swift
+++ Maple-Quest/Map/LocationDetailView.swift
@@ -11,14 +11,10 @@ import CoreLocation
 
 struct LocationDetailView: View {
     
+    // Variables
     var landmark: Landmark
     let userLocation: CLLocationCoordinate2D?
     var onVisited: (Bool) -> Void
-    
-    // Access AuthService to get the current user's email for saving photos securely
-    @EnvironmentObject var authService: AuthService
-    @Environment(\.dismiss) private var dismiss
-    
     @State private var galleryImages: [UIImage] = []
     @State private var selectedCameraOption: String?
     @State private var photosPickerItem: PhotosPickerItem?
@@ -26,12 +22,14 @@ struct LocationDetailView: View {
     @State private var showPhotoGallery: Bool = false
     @State private var selectedImage: UIImage?
     @State private var showCamera: Bool = false
-    
     private var visitedLandmark: Bool {
         !galleryImages.isEmpty
     }
     
-    // Calculate the distance from the landmark in meters
+    // Access AuthService to get the current user's email for saving photos securely
+    @EnvironmentObject var authService: AuthService
+    
+    // Calculates the user's distance from the landmark in meters
     private var distanceFromLandmark: CLLocationDistance? {
         guard let userLocation = userLocation else { return nil }
         let landmarkLocationCL = CLLocation(latitude: landmark.location.latitude, longitude: landmark.location.longitude)
@@ -45,23 +43,36 @@ struct LocationDetailView: View {
         return distance <= 500 // 500 meters is the required proximity
     }
     
-    // Main body of the view
+    // View
     var body: some View {
+        
+        // Landmark photo, name, location and description
         locationDetailContent
             .ignoresSafeArea(edges: .top)
             .toolbar {
+                
+                // Buttons for photo uploads and landmark gallery
                 toolbarContent
             }
+        
+            // Shows camera when selecting 'Take Photo'
             .fullScreenCover(isPresented: $showCamera) {
                 CameraView(image: $selectedImage)
                     .ignoresSafeArea(edges: .all)
             }
+        
+            // Handles photo upload when taking a photo
             .onChange(of: selectedImage) { _, newItem in
                 if let newItem {
                     handlePhotoUpload(image: newItem)
                 }
             }
+        
+        
+            // Shows photo library when selecting 'Choose Photo'
             .photosPicker(isPresented: $showPhotoPicker, selection: $photosPickerItem, matching: .images)
+            
+            // Handles photo upload when choosing a photo
             .onChange(of: photosPickerItem) { _, newItem in
                 Task {
                     if let newItem, let data = try? await newItem.loadTransferable(type: Data.self), let image = UIImage(data: data) {
@@ -70,19 +81,23 @@ struct LocationDetailView: View {
                     photosPickerItem = nil
                 }
             }
+        
             .navigationDestination(isPresented: $showPhotoGallery) {
+                
                 // Calls the PhotoGallery with the onDelete closure
                 PhotoGallery(images: $galleryImages) { imageToDelete in
                     deleteImage(imageToDelete)
                 }
                 .navigationTitle("Gallery")
             }
+        
+            // Displays images uploaded in the gallery to each landmark
             .onAppear {
                 galleryImages = loadImages(for: landmark.name)
             }
     }
     
-    // Landmark Detail Content
+    // Landmark detail content view
     private var locationDetailContent: some View {
         ZStack {
             Color(hex: "EAF6FF")
@@ -91,7 +106,8 @@ struct LocationDetailView: View {
                 VStack {
                     ZStack {
                         GeometryReader { geometry in
-                            // Image Section
+                            
+                            // Polaroid template
                             Image("polaroid")
                                 .resizable()
                                 .scaledToFill()
@@ -100,6 +116,7 @@ struct LocationDetailView: View {
                                 .clipped()
                                 .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
                             
+                            // Landmark image
                             Image(landmark.imageName)
                                 .resizable()
                                 .scaledToFill()
@@ -108,7 +125,8 @@ struct LocationDetailView: View {
                                 .offset(x: 48, y: 35)
                             
                             VStack {
-                                // Title
+                                
+                                // Landmark name
                                 Text(landmark.name)
                                     .font(.title3)
                                     .fontWeight(.bold)
@@ -116,7 +134,7 @@ struct LocationDetailView: View {
                                     .padding(.top, 65)
                                     .offset(y: 300)
                                 
-                                // Location
+                                // Landmark location
                                 Text("\(landmark.province), \(landmark.country)")
                                     .font(.system(size: 16))
                                     .foregroundColor(.gray)
@@ -126,9 +144,12 @@ struct LocationDetailView: View {
                         }
                     }
                     .frame(height: 450)
-                    .padding(.top, 120) // Padding between the top bar and the polaroid
+                    
+                    // Padding between the top bar and the polaroid
+                    .padding(.top, 120)
                 }
-                // Info Section
+                
+                // Displays text indicating whether or not the user has visited that landmark before
                 VStack(alignment: .leading) {
                     if visitedLandmark {
                         Text("You have visited this landmark before!")
@@ -147,15 +168,18 @@ struct LocationDetailView: View {
                     
                     Divider()
                         .padding(5)
+                    
+                    // Landmark description
                     Text("Details")
                         .font(.title2).bold()
-                    // Description
                     Text(landmark.description)
                         .font(.system(size: 17))
                         .foregroundColor(.black.opacity(0.6))
                         .padding(.top, 5)
+                    
                     Divider()
-                    // Proximity Check
+                    
+                    // Checks how close the user is to the landmark
                     if let distance = distanceFromLandmark {
                         HStack {
                             Image(systemName: isUserNearby ? "checkmark.circle.fill" : "xmark.circle.fill")
@@ -178,9 +202,11 @@ struct LocationDetailView: View {
         }
     }
     
-    // Toolbar Content
+    // Toolbar content
     @ToolbarContentBuilder
     private var toolbarContent: some ToolbarContent {
+        
+        // Menu to upload an image to the landmark gallery
         ToolbarItem(placement: .topBarTrailing) {
             Menu {
                 Button {
@@ -198,8 +224,12 @@ struct LocationDetailView: View {
             } label: {
                 Image(systemName: "plus.circle.fill")
             }
-            .disabled(!isUserNearby) // This feature is disabled if the user is not nearby
+            
+            // The feature to upload a photo at a landmark is disabled if the user is not within 500 meters.
+            .disabled(!isUserNearby)
         }
+        
+        // Button to view the landmark gallery
         ToolbarItem(placement: .topBarTrailing) {
             Button {
                 showPhotoGallery = true
@@ -209,6 +239,7 @@ struct LocationDetailView: View {
         }
     }
     
+    // Function that handles uploading a photo and adds it to the landmark gallery
     private func handlePhotoUpload(image: UIImage) {
         galleryImages.append(image)
         saveImages(galleryImages, for: landmark.name)
@@ -216,25 +247,24 @@ struct LocationDetailView: View {
         showPhotoGallery = true
     }
     
+    // Function that deletes an image from the gallery
     private func deleteImage(_ image: UIImage) {
         galleryImages.removeAll { $0 == image }
         saveImages(galleryImages, for: landmark.name)
     }
 
-    // MARK: - User Specific Storage
-    
+    // Function that saves the images uploaded by the user in each landmark so it persists between app launches
     func saveImages(_ images: [UIImage], for landmarkName: String) {
         guard let userId = authService.currentUser?.email else { return }
         let key = "photos_\(userId)_\(landmarkName)"
-        
         let imageDataArray = images.compactMap { $0.jpegData(compressionQuality: 0.8) }
         UserDefaults.standard.set(imageDataArray, forKey: key)
     }
 
+    // Function that retrieves saved images in each landmark and displays them in the gallery
     func loadImages(for landmarkName: String) -> [UIImage] {
         guard let userId = authService.currentUser?.email else { return [] }
         let key = "photos_\(userId)_\(landmarkName)"
-        
         guard let imageDataArray = UserDefaults.standard.array(forKey: key) as? [Data] else {
             return []
         }
