b43d3357370d87d7cb7bf7304c9c733c14bc52e7
Camila Hernandez <camila.hernandez@ucalgary.ca>
2025-11-21 16:38:06 -0700

    Added comments to CameraView.swift

--- Maple-Quest/Map/CameraView.swift
+++ Maple-Quest/Map/CameraView.swift
@@ -10,41 +10,45 @@ import SwiftUI
 import UIKit
 
 struct CameraView: UIViewControllerRepresentable {
+    
+    // Variables
     @Binding var image: UIImage?
     @Environment(\.presentationMode) var presentationMode
     
+    // Creates and configures the UIImagePickerController
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
         picker.sourceType = .camera
-        // If you want to allow cropping/editing, set this to true:
-        // picker.allowsEditing = true
         return picker
     }
     
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
-        
-    }
+    // Updates the controller
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) { }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
+    // Handles camera actions
     class Coordinator : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        // Variables
         let parent: CameraView
         
         init(_ parent: CameraView) {
             self.parent = parent
         }
         
+        // Called when the user takes a photo
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            // FIX: Change .editedImage to .originalImage
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                 parent.image = image
             }
             parent.presentationMode.wrappedValue.dismiss()
         }
         
+        // Called when the user cancels the camera
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             parent.presentationMode.wrappedValue.dismiss()
         }
